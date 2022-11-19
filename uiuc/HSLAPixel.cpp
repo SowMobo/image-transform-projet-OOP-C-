1ff43470b86cac92e90364c1f1ea35159f80a240
SowModibo <sowmodibo62@gmail.com>
2022-11-19 22:40:27 +0100

    image transform project by OOP

---
+++ uiuc/HSLAPixel.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file HSLAPixel.cpp
+ * Implementation of the HSLAPixel class for use in with the PNG library.
+ *
+ * @author University of Illinois CS 225 Course Staff
+ * @version 2018r1-lab1 - Updated for CS 400
+ */
+
+#include <cmath>
+#include <iostream>
+#include "HSLAPixel.h"
+using namespace std;
+
+namespace uiuc {
+    HSLAPixel::HSLAPixel(double hue, double saturation, double luminance, double alpha) 
+        :h{hue}, s{saturation}, l{luminance}, a{alpha}
+        {}
+        
+    std::ostream& operator<<(std::ostream &out, const HSLAPixel &pixel) {
+        out << "Pixel(h = " << pixel.h << ", s = " << pixel.s << ", l = " << pixel.l << ", a = " << pixel.a << ")\n";
+        return out;
+    }
+}
